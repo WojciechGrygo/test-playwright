bca8064d686f20fd202c24b9cfd56a256108d658
WojciechGrygo <grygo.wojtek@gmail.com>
2025-04-09 10:59:26 +0200

    Refactor user data variable naming in GAD-R03 tests for clarity

--- tests/ui/GAD-R03.spec.ts
+++ tests/ui/GAD-R03.spec.ts
@@ -3,11 +3,11 @@ import { RegisterPage } from '../../src/pages/register.page';
 import { generateUserData } from '../../src/utils/generator';
 import test from '@playwright/test';
 
-let user;
+let randomUserData;
 let registerPage: RegisterPage;
 
 test.beforeEach(async ({ page }) => {
-  user = generateUserData(); // Generate user data
+  randomUserData = generateUserData(); // Generate user data
   registerPage = new RegisterPage(page);
 });
 
@@ -15,17 +15,17 @@ test('@GAD-R03-01 User can register to the service using required fields', async
   const loginPage = new LoginPage(page);
 
   await registerPage.goto();
-  await registerPage.fillForm(user);
+  await registerPage.fillForm(randomUserData);
   await registerPage.clickRegisterButton();
   await registerPage.verifyAlertPopup('User created');
   await loginPage.verifyPageTitle();
 });
 
 test('@GAD-R03-02 When user provide incorrect email, message "Please provide a valid email address" is presented', async () => {
-  user.email = 'invalidEmail'; // Set an invalid email
+  randomUserData.email = 'invalidEmail'; // Set an invalid email
 
   await registerPage.goto();
-  await registerPage.fillForm(user);
+  await registerPage.fillForm(randomUserData);
   await registerPage.clickRegisterButton();
   await registerPage.verifyEmailError('Please provide a valid email address');
 });
